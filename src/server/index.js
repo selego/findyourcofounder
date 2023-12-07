401f36cec1dbc79bb5bf7174f2173de6b6796bbc
naei <10995534+naei@users.noreply.github.com>
2023-12-07 11:25:35 +0100

    starts on 0.0.0.0

--- src/server/index.js
+++ src/server/index.js
@@ -3,7 +3,7 @@ const { parse } = require("url");
 const next = require("next");
 
 const dev = process.env.NODE_ENV !== "production";
-const hostname = "localhost";
+const hostname = "0.0.0.0";
 const port = 8080;
 const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
@@ -24,5 +24,3 @@ app.prepare().then(() => {
     console.log(`> Ready on http://${hostname}:${port}`);
   });
 });
-
-
