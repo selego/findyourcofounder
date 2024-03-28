136b4847709486c1e4f2fdb04c208150ba9bf69f
Jawad UL Hassan <jwadulhssn@gmail.com>
2024-03-28 10:13:39 +0500

    a fix

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -13,6 +13,7 @@ import { authOptions } from "./api/auth/[...nextauth]/route";
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  display: "swap",
 });
 
 export const metadata = {
