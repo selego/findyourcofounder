bcb29795a831b8c7be3ac4837b20352840afeaee
Jawad UL Hassan <jwadulhssn@gmail.com>
2024-03-28 10:41:09 +0500

    preload

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -14,8 +14,7 @@ const poppins = Poppins({
   subsets: ["latin"],
   variable: '--font-poppins',
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-  adjustFontFallback: false,
-  display: "swap",
+  preload: false,
 });
 
 export const metadata = {
