f35f0a63f345cf918dc412f276857d468bb360d1
azizmobarak <mobarakaziz9@gmail.com>
2024-07-17 13:10:41 +0100

    change coutry dynamicly

--- src/app/config.js
+++ src/app/config.js
@@ -1,1 +1,15 @@
-export const APP_COUNTRY = "es";
+export const APP_COUNTRY = getCountryCodeFromUrl();
+
+
+function getCountryCodeFromUrl(url) {
+  try {
+    const urlObj = new URL(url);
+    const hostname = urlObj.hostname;
+    const parts = hostname.split(".");
+    const tld = parts[parts.length - 1];
+
+    return tld;
+  } catch () {
+    return 'es';
+  }
+}
