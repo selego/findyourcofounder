71db3419a62b52bd38495a985d3b5ec7f95abf19
Jawad ul hassan <jwadulhssn@gmail.com>
2024-08-28 20:22:07 +0500

    basic

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -3,70 +3,59 @@
 import { httpService } from "@/services/httpService";
 import { APP_COUNTRY } from "@/app/config";
 
-function SiteMap({ data }) {
+// Sitemap configuration
+export const dynamic = 'force-dynamic'; // To ensure dynamic generation of the sitemap
+
+export default async function getSitemap() {
+  const { ok, data } = await httpService.post(`/search?timestamp=${new Date().getTime()}`);
+
   const extension = APP_COUNTRY === "nl" ? APP_COUNTRY : "es";
 
-  const arr = [];
-  arr.push({
-    url: `https://findyourcofounder.${extension}`,
-    lastModified: new Date(),
-    changeFrequency: "weekly",
-    priority: 1,
-  });
-  arr.push({
-    url: `https://findyourcofounder.${extension}/gdpr`,
-    lastModified: new Date(),
-    changeFrequency: "weekly",
-    priority: 1,
-  });
-  arr.push({
-    url: `https://findyourcofounder.${extension}/gdpr`,
-    lastModified: new Date(),
-    changeFrequency: "weekly",
-    priority: 1,
-  });
-  arr.push({
-    url: `https://findyourcofounder.${extension}/concept`,
-    lastModified: new Date(),
-    changeFrequency: "weekly",
-    priority: 1,
-  });
-  arr.push({
-    url: `https://findyourcofounder.${extension}/legale`,
-    lastModified: new Date(),
-    changeFrequency: "weekly",
-    priority: 1,
-  });
+  // Array to hold all URLs
+  const urls = [
+    {
+      url: `https://findyourcofounder.${extension}`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: `https://findyourcofounder.${extension}/gdpr`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: `https://findyourcofounder.${extension}/concept`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    },
+    {
+      url: `https://findyourcofounder.${extension}/legale`,
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1,
+    }
+  ];
 
-  for (item of data) {
-    arr.push({
-      url: `https://findyourcofounder.${extension}/contact/${item._id}`,
+  // Loop through dynamic users and add to URLs array
+  (data?.users || []).forEach((item) => {
+    urls.push({
+      url: `https://findyourcofounder.${extension}/contact/${item.slug}`,
       lastModified: new Date(),
       changeFrequency: "weekly",
       priority: 1,
     });
-  }
+  });
 
-  return arr;
+  return urls
+
+  // Return the correct structure for the sitemap
+  // return urls.map(({ url, lastModified, changeFrequency, priority }) => ({
+  //   loc: url,
+  //   lastmod: lastModified.toISOString(),
+  //   changefreq: changeFrequency,
+  //   priority,
+  // }));
 }
-
-export async function getServerSideProps({ res }) {
-  // We make an API call to gather the URLs for our site
-  const { ok, data } = await httpService.post(`/search?timestamp=${new Date().getTime()}`);
-
-  // console.log(data.users.length)
-
-  // // We generate the XML sitemap with the data
-  // const sitemap = generateSiteMap(data?.users || []);
-
-  // res.setHeader("Content-Type", "text/xml");
-  // // we send the XML to the browser
-  // res.write(sitemap);
-  // res.end();
-
-  return {
-    props: { data: data.users },
-  };
-}
-
-export default SiteMap;
