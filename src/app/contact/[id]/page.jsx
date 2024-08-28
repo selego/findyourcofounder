9318969f343a1cb2f491deb8f3e3a94df1e1d33c
Jawad ul hassan <jwadulhssn@gmail.com>
2024-08-28 16:06:21 +0500

    up pages

---
+++ src/app/contact/[id]/page.jsx
@@ -0,0 +1,64 @@
+import Link from "next/link";
+import { FaLinkedin } from "react-icons/fa6";
+import { skillsColors } from "@/app/utils/constants";
+import { httpService } from "@/services/httpService";
+
+export default async function Contact({ params }) {
+  const { ok, data } = await httpService.get(`/${params.id}`);
+  if(!data) return <></>
+
+  return (
+    <>
+      <main className="max-w-[750px] w-full mx-auto flex flex-col gap-y-12">
+        <article className="flex flex-col gap-y-12">
+          <h1 className="text-center text-shadow lg:text-2xl text-xl">
+            {data.first_name} {data.last_name}
+            {data.linkedin && (
+              <Link href={data.linkedin} target="_blank" className="lg:ml-4 ml-2.5">
+                <FaLinkedin className="inline-block text-linkedIn" />
+              </Link>
+            )}
+          </h1>
+
+          <ul className="flex items-center pb-1 pt-4 gap-x-2 overflow-x-scroll masked-skills">
+            {data.skills?.map((skill) => (
+              <li
+                key={skill}
+                className={`text-xs py-1 px-4 rounded-full 
+              ${skillsColors[skill]}`}
+              >
+                {skill}
+              </li>
+            ))}
+          </ul>
+
+          <div className="flex flex-col lg:gap-y-12 gap-y-6 flex-1 overflow-hidden mask-desc">
+            <div>
+              <h3 className="opacity-50 lg:text-xs text-xxs">What motivates you</h3>
+              <p className="text-xs lg:text-sm">&quot;{data.motivations}&quot;</p>
+            </div>
+            <div>
+              <h3 className="opacity-50 lg:text-xs text-xxs">The ideal partner</h3>
+              <p className="text-xs lg:text-sm">&quot;{data.partner}&quot;</p>
+            </div>
+            <div>
+              <h3 className="opacity-50 lg:text-xs text-xxs">The dream business</h3>
+              <p className="text-xs lg:text-sm">&quot;{data.business}&quot;</p>
+            </div>
+          </div>
+
+          <div className="flex items-center justify-between pt-2 gap-6">
+            <div className="overflow-hidden">
+              <h5 className="text-sm whitespace-nowrap">City</h5>
+              <p className="truncate text-yellow">{data.city}</p>
+            </div>
+            <div>
+              <h5 className="text-sm whitespace-nowrap">Investment</h5>
+              <p className="truncate text-yellow text-end">{data.invest} €</p>
+            </div>
+          </div>
+        </article>
+      </main>
+    </>
+  );
+}
--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -3,9 +3,27 @@
 import { httpService } from "@/services/httpService";
 import { APP_COUNTRY } from "@/app/config";
 
-
 function generateSiteMap(data) {
-    const extension = APP_COUNTRY === "nl" ? APP_COUNTRY : "es"
+  const extension = APP_COUNTRY === "nl" ? APP_COUNTRY : "es";
+
+  console.log(`<?xml version="1.0" encoding="UTF-8"?>
+   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+     <!--We manually set the two URLs we know already-->
+     <url>
+       <loc>https://findyourcofounder.${extension}</loc>
+     </url>
+     <url>
+       <loc>https://findyourcofounder.${extension}/gdpr</loc>
+     </url>
+     <url>
+       <loc>https://findyourcofounder.${extension}/concept</loc>
+     </url>
+     <url>
+       <loc>https://findyourcofounder.${extension}/legale</loc>
+     </url>
+     </urlset>
+ `)
+
   return `<?xml version="1.0" encoding="UTF-8"?>
    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
      <!--We manually set the two URLs we know already-->
@@ -21,20 +39,9 @@ function generateSiteMap(data) {
      <url>
        <loc>https://findyourcofounder.${extension}/legale</loc>
      </url>
-   </urlset>
+     </urlset>
  `;
 }
-// ${
-//     data
-//    .map(({ id }) => {
-//      return `
-//    <url>
-//        <loc>${`${EXTERNAL_DATA_URL}/${id}`}</loc>
-//    </url>
-//  `;
-//    })
-//    .join('')
-// }
 
 function SiteMap() {
   // getServerSideProps will do the heavy lifting
@@ -42,12 +49,14 @@ function SiteMap() {
 
 export async function getServerSideProps({ res }) {
   // We make an API call to gather the URLs for our site
-    // const { ok, data } = await httpService.post(`/search?timestamp=${new Date().getTime()}`);
+  const { ok, data } = await httpService.post(`/search?timestamp=${new Date().getTime()}`);
+
+  console.log(data.users.length)
 
   // We generate the XML sitemap with the data
-  const sitemap = generateSiteMap([]);
+  const sitemap = generateSiteMap(data?.users || []);
 
-  res.setHeader('Content-Type', 'text/xml');
+  res.setHeader("Content-Type", "text/xml");
   // we send the XML to the browser
   res.write(sitemap);
   res.end();
